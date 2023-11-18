66b5e1f4b4ecdc4ccacc82a907e39d58b9d67627
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-11-18 12:31:04 -0700

    added deployed links to projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,53 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
-import Ecommerce from '../../images/ecommerce.jpeg';
-import Employees from '../../images/employees.png';
-import Logo from '../../images/logo.jpeg';
-import Quiz from '../../images/quiz.webp';
-import Readme from '../../images/readme.png';
-import MTG from '../../images/knockoff-gatherer2.0.png';
+import Ecommerce from "../../images/ecommerce.jpeg";
+import Employees from "../../images/employees.png";
+import Logo from "../../images/logo.jpeg";
+import Quiz from "../../images/quiz.webp";
+import Readme from "../../images/readme.png";
+import MTG from "../../images/knockoff-gatherer2.0.png";
 import Navigation from "../components/Navigation";
+
+const Project = ({ title, repoLink, deployedLink, image }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className="col-md-6" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <p className="titles">{title}</p>
+      <div>
+        <a href={repoLink} target="_blank" rel="noopener noreferrer">
+          <img className="thumbnails" src={image} alt={`${title} repo`} />
+        </a>
+        {isHovered && (
+          <div className="links">
+            <p>
+              <a href={repoLink} target="_blank" rel="noopener noreferrer">
+                Repo
+              </a>{" "}
+              |{" "}
+              <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+                Deployed
+              </a>
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const Portfolio = () => {
   return (
     <div className="container">
-      <Navigation /> 
-      <h2 class="page-title">Highlighted Projects</h2>
+      <Navigation />
+      <h2 className="page-title">Highlighted Projects</h2>
       <p>These are six of my highlighted projects. All images are clickable links that will bring you to the GitHub Repository to view the code.</p>
 
       <div className="row">
-        {/* First Column */}
-              <div className="col-md-6">
-                  <p class="titles">Knockoff Gatherer</p>
-          <a href="https://github.com/JacobCampa/Knockoff-Gatherer-2.0" target="_blank" rel="noopener noreferrer">
-            <img className="thumbnails" src={MTG} alt="Knockoff Gatherer2.0 repo" />
-          </a>
-          <p class="titles">Ecommerce backend</p>
-          <a href="https://github.com/AnthonyCiccone90/ecommerce-backend" target="_blank" rel="noopener noreferrer">
-            <img className="thumbnails" src={Ecommerce} alt="Ecommerce repo" />
-          </a>
-          <p class="titles">Employee Content Management System</p>
-          <a href="https://github.com/AnthonyCiccone90/employee_cms" target="_blank" rel="noopener noreferrer">
-            <img className="thumbnails" src={Employees} alt="Employees CMS repo" />
-          </a>
-          <p class="titles">Logo Creator</p>
-        </div>
-
-        {/* Second Column */}
-        <div className="col-md-6">
-          <a href="https://github.com/AnthonyCiccone90/logo-maker" target="_blank" rel="noopener noreferrer">
-            <img className="thumbnails" src={Logo} alt="Logo generator repo" />
-          </a>
-          <p class="titles">Coding Quiz</p>
-          <a href="https://github.com/AnthonyCiccone90/coding-quiz" target="_blank" rel="noopener noreferrer">
-            <img className="thumbnails" src={Quiz} alt="Quiz Repo" />
-          </a>
-          <p class="titles">README Generator</p>
-          <a href="https://github.com/AnthonyCiccone90/coding-quiz" target="_blank" rel="noopener noreferrer">
-            <img className="thumbnails" src={Readme} alt="Readme repo" />
-                  </a>
-        </div>
+        <Project title="Knockoff Gatherer" repoLink="https://github.com/JacobCampa/Knockoff-Gatherer-2.0" deployedLink="https://mtg-search-engine-9599f588e5fc.herokuapp.com/" image={MTG} />
+        <Project title="Ecommerce backend" repoLink="https://github.com/AnthonyCiccone90/ecommerce-backend" deployedLink="https://drive.google.com/file/d/1j2Bn5AuWcuArkWzT23UhcYBFUxkJYiks/view" image={Ecommerce} />
+        <Project title="Employee Content Management System" repoLink="https://github.com/AnthonyCiccone90/employee_cms" deployedLink="https://drive.google.com/file/d/1bngbfbpQRfUur0G4UknP8OFGjoCUSU8L/view" image={Employees} />
+        <Project title="Logo Creator" repoLink="https://github.com/AnthonyCiccone90/logo-maker" deployedLink="https://drive.google.com/file/d/1nCZox-ecSC5MM4k2qze31N6Dkkg0QujG/view" image={Logo} />
+        <Project title="Coding Quiz" repoLink="https://github.com/AnthonyCiccone90/coding-quiz" deployedLink="https://anthonyciccone90.github.io/coding-quiz/" image={Quiz} />
+        <Project title="README Generator" repoLink="https://github.com/AnthonyCiccone90/coding-quiz" deployedLink="https://anthonyciccone90.github.io/coding-quiz/" image={Readme} />
       </div>
 
       <Footer />
